9071dc11456c9bb095dd0ccd25c8461b22a877d1
eystdahl <93379583+eystdahl@users.noreply.github.com>
2026-04-13 17:02:46 +0200

    Add files via upload

--- TransitivityProminence_PaVeDa_final.R
+++ TransitivityProminence_PaVeDa_final.R
@@ -1,6 +1,6 @@
 #Make matrix of transitivity prominence across IE in PaVeDa data with languages in chronological order
 
-TransitivityProminence_PaVeDa <- matrix(c(78, 73, 78, 73, 80, 83, 73, 66, 73, 71), ncol = 10, byrow = TRUE)
+TransitivityProminence_PaVeDa <- matrix(c(78, 73, 78, 76, 80, 83, 73, 66, 73, 71), ncol = 10, byrow = TRUE)
 
 colnames(TransitivityProminence_PaVeDa) <- c("Hittite", "Vedic Sanskrit", "Avestan", "Ancient Greek", "Old Latin", "Old Irish", "Classical Armenian", "Old Church Slavonic", "Old Norse", "Old Lithuanian")
 
@@ -15,15 +15,15 @@ box()
 
 #Make matrix of transitivity prominence across IE in PaVeDa data with languages in decreasing order
 
-TransitivityProminence_PaVeDa_decreasing <- matrix(c(83, 80, 78, 78, 73, 73, 73, 73, 71, 66), ncol = 10, byrow = TRUE)
+TransitivityProminence_PaVeDa_decreasing <- matrix(c(83, 80, 78, 78, 76, 73, 73, 73, 71, 66), ncol = 10, byrow = TRUE)
 
-colnames(TransitivityProminence_PaVeDa_decreasing) <- c("Old Irish", "Old Latin", "Hittite", "Avestan", "Vedic Sanskrit", "Homeric Greek", "Classical Armenian", "Old Norse", "Old Lithuanian", "Old Church Slavonic")
+colnames(TransitivityProminence_PaVeDa_decreasing) <- c("Old Irish", "Old Latin", "Hittite", "Avestan", "Homeric Greek", "Vedic Sanskrit", "Classical Armenian", "Old Norse", "Old Lithuanian", "Old Church Slavonic")
 
 TransitivityProminence_PaVeDa_decreasing
 
 #Make barplot
-
-barplot(TransitivityProminence_PaVeDa_decreasing, ylim = range(0,100), xlab = "Languages", ylab = "Relative frequency (%)", col = c("black"), beside = TRUE)
+par(oma = c(4, 2, 0, 2))
+barplot(TransitivityProminence_PaVeDa_decreasing, las = 2, ylim = range(0,100), xlab = "", ylab = "Relative frequency (%)", col = c("black"), beside = TRUE)
 box()
 
 
@@ -32,7 +32,7 @@ library("phangorn")
 
 #NeighborNet plot of PaVeDa data
 
-PaVeDa_verbs <- matrix(c(1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1), ncol = 41, byrow = TRUE)
+PaVeDa_verbs <- matrix(c(1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1), ncol = 41, byrow = TRUE)
 rownames(PaVeDa_verbs) <- c("Hittite", "Vedic", "Avestan", "Homeric Greek", "Old Latin", "Old Irish", "Classical Armenian", "Old Church Slavonic", "Old Norse", "Old Lithuanian")
 colnames(PaVeDa_verbs) <- c("ASK FOR", "BEAT", "BREAK", "BUILD", "CARRY", "COVER", "CUT", "DIE", "DRESS", "EAT", "FEAR", "FILL", "FRIGHTEN", "GIVE", "GO", "HEAR", "HELP", "HIDE", "KILL", "KNOW", "LEAVE", "LIKE", "LIVE", "LOOK AT", "NAME", "POUR", "PUT", "RUN", "SAY", "SEARCH FOR", "SEE", "SEND", "SHOW", "SING", "SIT", "SIT DOWN", "TAKE", "THROW", "TIE", "TOUCH", "WASH")
 PaVeDa_verbs
@@ -47,17 +47,18 @@ plot(netPaVeDa_verbs)
 
 #Make matrix of transitivity prominence in IE based on ASTRAPIE data with languages in chronological order
 
-TransitivityProminence_ASTRAPIE <- matrix(c(63, 43, 43, 63, 40, 40, 30, 23, 33, 50, 36), ncol = 11, byrow = TRUE)
+TransitivityProminence_ASTRAPIE <- matrix(c(60, 43, 47, 63, 40, 40, 30, 23, 33, 50, 36), ncol = 11, byrow = TRUE)
 colnames(TransitivityProminence_ASTRAPIE) <- c("Hittite", "Vedic", "Homeric Greek", "Old Latin", "Early Irish", "Classical Armenian", "Tocharian", "Old Church Slavonic", "Old Norse", "Old Albanian", "Old Lithuanian")
 TransitivityProminence_ASTRAPIE
 
 #Make matrix of transitivity prominence in IE based on ASTRAPIE data with languages in decreasing order
 
-TransitivityProminence_ASTRAPIE_decreasing <- matrix(c(63, 63, 50, 43, 43, 40, 40, 36, 33, 30, 23), ncol = 11, byrow = TRUE)
-colnames(TransitivityProminence_ASTRAPIE_decreasing) <- c("Hittite", "Old Latin", "Old Albanian", "Vedic", "Homeric Greek", "Old Irish", "Classical Armenian", "Old Lithuanian", "Old Norse", "Tocharian", "Old Church Slavonic")
+TransitivityProminence_ASTRAPIE_decreasing <- matrix(c(63, 60, 50, 47, 43, 40, 40, 36, 33, 30, 23), ncol = 11, byrow = TRUE)
+colnames(TransitivityProminence_ASTRAPIE_decreasing) <- c("Old Latin", "Hittite", "Old Albanian", "Homeric Greek", "Vedic", "Old Irish", "Classical Armenian", "Old Lithuanian", "Old Norse", "Tocharian", "Old Church Slavonic")
 TransitivityProminence_ASTRAPIE_decreasing
 
-barplot(TransitivityProminence_ASTRAPIE_decreasing, ylim = range(0,100), xlab = "Languages", ylab = "Relative frequency (%)", col = c("black"), beside = TRUE)
+par(oma = c(4, 2, 0, 2))
+barplot(TransitivityProminence_ASTRAPIE_decreasing, las = 2, ylim = range(0,100), xlab = "", ylab = "Relative frequency (%)", col = c("black"), beside = TRUE)
 box()
 
 #Make neighborNet plot of ASTRAPIE data
@@ -65,7 +66,7 @@ box()
 
 #Factoring in voice marking
 
-ASTRAPIE_verbs <- matrix(c(1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0), ncol = 30, byrow = TRUE)
+ASTRAPIE_verbs <- matrix(c(1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0), ncol = 30, byrow = TRUE)
 rownames(ASTRAPIE_verbs) <- c("Hittite", "Vedic", "Homeric Greek", "Old Latin", "Old Irish", "Classical Armenian", "Tocharian", "Old Church Slavonic", "Old Norse", "Old Albanian", "Old Lithuanian")
 
 ASTRAPIE_verbs
@@ -90,8 +91,8 @@ colnames(TransitivityProminence_ASTRAPIE_alt_decreasing) <- c("Hittite", "Old Ir
 TransitivityProminence_ASTRAPIE_alt_decreasing
 
 #Make barplot
-
-barplot(TransitivityProminence_ASTRAPIE_alt_decreasing, ylim = range(0,100), xlab = "Languages", ylab = "Relative frequency (%)", col = c("black"), beside = TRUE)
+par(oma = c(4, 2, 0, 2))
+barplot(TransitivityProminence_ASTRAPIE_alt_decreasing, las = 2, ylim = range(0,100), xlab = "", ylab = "Relative frequency (%)", col = c("black"), beside = TRUE)
 box()
 
 
